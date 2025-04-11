40ee119df55abc11b923d6869cf8d59a1487e060
spgpu <132106900+keepflowing@users.noreply.github.com>
2025-04-11 09:29:41 -0500

    Refactor Memory

--- dmg-core/src/memory.rs
+++ dmg-core/src/memory.rs
@@ -1,24 +1,33 @@
-pub trait Memory {
-    fn read(&self, address: u16) -> u8;
-    fn write(&mut self, address: u16, value: u8);
+/// Holds the different memory regions
+#[derive(Debug, Clone, Copy)]
+pub struct MemoryBus {
+    // See https://gbdev.io/pandocs/Memory_Map.html
+    // Ignores unused / GBC regions
+    rom_bank_00: [u8; 16 * 1024],
+    rom_bank_nn: [u8; 16 * 1024],
+    vram: [u8; 8 * 1024],
+    ext_ram: [u8; 8 * 1024],
+    wram_0: [u8; 4 * 1024],
+    wram_n: [u8; 4 * 1024],
+    oam: [u8; 160],
+    io_registers: [u8; 128],
+    hram: [u8; 127],
+    interrupt: u8,
 }
 
-pub struct SimpleMemory {
-    data: [u8; 0x10000], // 64KB
-}
-
-impl SimpleMemory {
+impl MemoryBus {
     pub fn new() -> Self {
-        SimpleMemory { data: [0; 0x10000] }
-    }
-}
-
-impl Memory for SimpleMemory {
-    fn read(&self, address: u16) -> u8 {
-        self.data[address as usize]
-    }
-
-    fn write(&mut self, address: u16, value: u8) {
-        self.data[address as usize] = value;
+        Self {
+            rom_bank_00: [0; 16 * 1024],
+            rom_bank_nn: [0; 16 * 1024],
+            vram: [0; 8 * 1024],
+            ext_ram: [0; 8 * 1024],
+            wram_0: [0; 4 * 1024],
+            wram_n: [0; 4 * 1024],
+            oam: [0; 160],
+            io_registers: [0; 128],
+            hram: [0; 127],
+            interrupt: 0,
+        }
     }
 }
